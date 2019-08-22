3b10116094a520362fa481137d2e870e5c9428eb
Juan Manuel Valencia <roumin@protonmail.com>
2019-08-21 20:49:48 -0500

    first commit

---
+++ API.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import datetime
+import random
+
+app = Flask(__name__)
+CORS(app)
+
+tipo_medicion = {'sensor':'PH0-14', 'variable':'PH', 'unidades':'Nivel de PH'}
+
+mediciones=[
+    {'fecha':datetime.datetime.now(), **tipo_medicion, 'valor':random.randint(5,11)},
+    {'fecha':datetime.datetime.now(), **tipo_medicion, 'valor':random.randint(5,11)},
+    {'fecha':datetime.datetime.now(), **tipo_medicion, 'valor':random.randint(5,11)},
+    {'fecha':datetime.datetime.now(), **tipo_medicion, 'valor':random.randint(5,11)},
+    {'fecha':datetime.datetime.now(), **tipo_medicion, 'valor':random.randint(5,11)},
+    {'fecha':datetime.datetime.now(), **tipo_medicion, 'valor':random.randint(5,11)}
+]
+
+@app.route('/', methods=['GET'])
+def getAll():
+    return jsonify(mediciones)
+
+@app.route('/mayoresque/<string:date>', methods=['GET'])
+def getMayoresA(date):
+    return date;
+
+app.run(port=80, debug=True)
